ae861830262e37a2840a609471ebf1e69c8484d9
Engr Saqib Jameel <65038331+saqib2611@users.noreply.github.com>
2020-07-18 14:39:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,423 @@
+
+// //chp 5 ques#3
+// var a = ' Value after variable declaration is undefined   <br>';
+//   document.write(a);
+// var a = ' Initial value : 5 <br>'
+// document.write(a);
+// var a = ' Value after increment is: '
+// document.write(a);
+// var a = 5;
+// var b = 1;
+// var c = a+b;
+// document.write(c);
+// var a = ' Value after addition is: '
+// document.write(a);
+// var a = 6;
+// var b = 7;
+// var c = a+b;
+// document.write(c);
+// var a = ' Value after decrement is: '
+// document.write(a);
+// var a = 13;
+// var b = 1;
+// var c = a-b;
+// document.write(c);
+
+// var a = ' The remainder is: '
+// document.write(a);
+// var a = 12;
+// var b = 0;
+// var c = a*b;
+// document.write(c);
+
+
+//chp 5 ques#4
+// var cos = ' Total cost to buy 5 tickets to a movie is '
+// var a = 600;
+// var b = 5;
+// var c = a*b;
+
+// document.write(cos);
+// document.write(c);
+
+
+//chp 5 ques#5
+// var a = '<h1> Table of 4 </h1>' + ' <br>';
+// document.write(a)
+// for (var i=1; i<11; i++){
+//     document.write('2' + 'x' + i + '=' + 2*i + '<br>')
+// }
+
+//chp 5 ques#6
+// var a = 68;
+// var b = 32;
+// var c = a-b;
+// var d = 5/9;
+// var e = c*d;
+// var f = ' °C is ';
+// var a = 60.5;
+// var b = 9/5;
+// var c = a*b;
+// var d = 32;
+// var g = c-d;
+// var h = ' °F <br>';
+// document.write(e);
+// document.write(f);
+// document.write(g);
+// document.write(h);
+// var a = 56.5;
+// var b = 9/5;
+// var c = a*b;
+// var d = 32;
+// var g = c-d;
+
+// var f = ' °F is ';
+// var a = 70;
+// var b = 32;
+// var c = a-b;
+// var d = 5/9;
+// var e = c*d;
+// var h = ' °C <br>';
+// document.write(g);
+// document.write(f);
+// document.write(e);
+// document.write(h);
+
+
+//chp 5 ques#7
+// var a = '<h1> Shopping Cart </h1>' + ' <br>';
+//  document.write(a)
+//  var  a = 'Price of item 1 is 650  <br> ';
+//  document.write(a)
+//  var c ='  Quantity of item 1 is  3 <br> ';
+//  document.write(c)
+//  var e = 'Price of item 2 is   100 <br>';
+//  document.write(e)
+//  var g = ' Quantity of item 2 is   7 <br> ';
+//  document.write(g)
+//  var  i = 'Shipping charges 100 <br> '
+//  document.write(i)
+//  var h = ' <h3> Total cost of your order is </h3>';
+//  var a = 650*3;
+//  var b = 100*7;
+//  var c = 100;
+//  var d = a+b+c  ;
+//  document.write(h );
+//  document.write(d)
+
+ 
+//chp 5 ques#8
+// var a = '<h1> Marks Sheet </h1>' + ' <br>';
+//  document.write(a)
+//  var a = ' Total marks : 980 <br>';
+//  document.write(a);
+//  var b = ' Marks obtained : 804 <br>';
+//  document.write(b);
+//  var c = 'Percentage : ';
+//  var d = 804/980;
+//  var e = 100;
+//  var f = d*e;
+//  document.write(c);
+//  document.write(f)
+
+//chp 5 ques#9
+// var a = '<h1> Currency in PKR </h1>' + ' <br>';
+//   document.write(a);
+//   var a = 'Total Currency in PKR : ';
+//   var b = 10*104.80;
+//   var c = 25*28;
+//   var d = b+c;
+//   document.write(a);
+//   document.write(d)
+
+//chp 5 ques#10
+//  var a = 2;
+//  var b = 3;
+//  var c = a+b   ;
+// var d = 10; 
+//  var e  = c*d;
+//  var f = 2;
+//  var g = e/f;
+// document.write(c);
+// document.write(e);
+//  document.write(g);
+
+//chp 5 ques#11
+// var a = '<h1> Age Calculator</h1>' + ' <br>';
+//  document.write(a);
+// var a = ' Current year : 2001 <br>';
+// document.write(a);
+// var b = ' Birth year : 2000 <br>';
+// document.write(b);
+// var a = ' Your age is :'
+// var b = 2020;
+// var c = 2001;
+// var d = b-c;
+// document.write(a);
+// document.write(d);
+
+//chp 5 ques#12
+// var a = '<h1> The Geometrizer</h1>' + ' <br>';
+//   document.write(a);
+//   var a = ' Radius of a cirlce is : 20 <br>';
+//  document.write(a);
+//  var a = 'The Circumference is :';
+//  var b = 2;
+//  var c = 3.142;
+//  var d = b*c;
+//  var e = 20;
+//  var f = d*e;
+//  document.write(a);
+//  document.write(f);
+//  var a = 'The area is :';
+//  var b =  3.142 ;
+//  var c =  20*20;
+//  var d = b*c;
+
+//  document.write(a);
+//  document.write(d);
+
+//chp 5 ques#13
+// var a = '<h1> The Lifetime Supply Calculator</h1>' + ' <br>';
+//   document.write(a);
+//   var a = ' Favourite Snack: kurlez<br>';
+//  document.write(a);
+//  var a = 'Current age : 19 <br>';
+//  document.write(a);
+//  var a = ' Estimated Maximum Age: 70 <br>'  ;
+//  document.write(a);
+//  var a = ' Amount of Snacks per day : 4 <br>';
+//  document.write(a);
+//  var a = '  You will need ';
+//  var b = 19;
+//  var c = 4;
+//  var d = b*c;
+//  var e = '  kurlez to last you until the ripe old age of 70 ';
+//  document.write(a);
+//  document.write(d);
+//  document.write(e)
+
+
+// //////////CHAPTER# 6  TO 9....
+///QUESTION#1.......
+
+// var a = '<h4> Result:</h4>' + ' <br>';
+//    document.write(a);
+// var a = 'The value of a is:  10 <br> ';
+// var b = '...............................<br> <br><br>';
+// document.write(a);
+// document.write(b);
+// var a = 'The value of a++ is: ';
+// var b = 10;
+// var c = 1 ;
+// var d = b+c;
+// document.write(a);
+// document.write(d);
+// var a = 'Now the  value of a is:  11<br><br><br> ';
+// document.write(a);
+// var a = 'The value of a++ is: ';
+// var b = 11;
+// var c = 1 ;
+// var d = b+c;
+// document.write(a);
+// document.write(d);
+// var a = 'Now the  value of a is:  12<br><br><br> ';
+// document.write(a);
+
+// var a = 'The value of --a is: ';
+// var b = 12;
+// var c = 1 ;
+// var d = b-c;
+// document.write(a);
+// document.write(d);
+// var a = 'Now the  value of a is:  11<br><br><br> ';
+// document.write(a);
+// var a = 'The value of a-- is: 11 <br> ';
+// document.write(a);
+// var a = 'Now the  value of a is:   ';
+// var b = 11;
+// var c = 1 ;
+// var d = b-c;
+// document.write(a);
+// document.write(d);
+
+//////QUESTION#2............
+// var a = 2;
+// var b = 1;
+// var c = ' --a : 1';
+// var d = ' --a - --b : 1';
+// var e = ' --a - --b + ++b : 2';
+// var f = ' --a - --b + ++b + b-- : 3';
+// var g = ' a is 2 <br>';
+// var h = ' b is 1 <br>';.
+// var i = ' result is ';
+// var Result = --a - --b + ++b + b--;
+// document.write(g);
+// document.write(h);
+// document.write(i);
+// document.write(Result)
+
+//////////QUESTION# 3..............
+// var name = ['Areez ' , 'Abiha', ' Ali' , ' Shafi' ];
+// alert('Welcome' + name);
+// document.write(name )
+
+/////////////QUESTION#5..........
+// var x = '';
+// var y = ' ';
+// var a = +prompt( x,y); 
+// var b = a*5;
+// document.write( ' The table is multiply by 5 and the result is' + '' + b)
+
+//////////////////QUESTION#6.............
+var arr = [ ' <h2> Subject  Total Marks  Obtained Marks  Percentage </h2> ' ]
+var arr1 = [ ' English  ' , '  100  ' , '  54'];
+document.write('<table>');
+for(var i = 0; i < arr.length; i++){
+    document.write( arr[i] )
+    document.write( arr1[i] )
+}
+document.write('</table>')
+
+
+
+//////////////////////////////CHAPTER # 10..........
+/////////////////QUESTION # 1
+// var a = +prompt( ); 
+
+// document.write( ' Welcome to city of lights!!!'  )
+
+////////////////////QUESTION # 2.........
+// var gender = prompt( ' Enter your gender!');
+
+// if( gender === 'male'){
+//     document.write('Good Morning Sir!!')
+// }else {
+// document.write('Good Morning Maam!!' )
+// }
+
+//   function tableCreate() {
+//     var body = document.getElementsByTagName('body')[0];
+//     var tbl = document.createElement('table');
+//     tbl.style.width = '100%';
+//     tbl.setAttribute('border', '1');
+//     var tbdy = document.createElement('tbody');
+//     for (var i = 0; i < 3; i++) {
+//       var tr = document.createElement('tr');
+//       for (var j = 0; j < 2; j++) {
+//         if (i == 2 && j == 1) {
+//           break
+//         } else {
+//           var td = document.createElement('td');
+//           td.appendChild(document.createTextNode('\u0020'))
+//           i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+//           tr.appendChild(td)
+//         }
+//       }
+//       tbdy.appendChild(tr);
+//     }
+//     tbl.appendChild(tbdy);
+//     body.appendChild(tbl)
+//   }
+//   tableCreate();
+
+//   function addTable() {
+//     var myTableDiv = document.getElementById("myDynamicTable");
+  
+//     var table = document.createElement('TABLE');
+//     table.border = '1';
+  
+//     var tableBody = document.createElement('TBODY');
+//     table.appendChild(tableBody);
+  
+//     for (var i = 0; i < 3; i++) {
+//       var tr = document.createElement('TR');
+//       tableBody.appendChild(tr);
+  
+//       for (var j = 0; j < 4; j++) {
+//         var td = document.createElement('TD');
+//         td.width = '75';
+//         td.appendChild(document.createTextNode("Cell " + i + "," + j));
+//         tr.appendChild(td);
+//       }
+//     }
+//     myTableDiv.appendChild(table);
+//   }
+//   addTable();
+//   <div id="myDynamicTable"></div>
+  
+
+
+
+{/* <table id="myTable" cellpadding="2" cellspacing="2" border="1" onclick="tester()"></table> */}
+{/* <script>
+    var student;
+    for (var j = 0; j < 10; j++) {
+        student = {
+            name: "Name" + j,
+            rank: "Rank" + j,
+            stuclass: "Class" + j,
+        };
+        var table = document.getElementById("myTable");
+        var row = table.insertRow(j);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+
+        cell1.innerHTML = student.name,
+        cell2.innerHTML = student.rank,
+        cell3.innerHTML = student.stuclass;
+    }
+</script> */}
+
+
+// function addCell(tr, val) {
+//     var td = document.createElement('td');
+
+//     td.innerHTML = val;
+
+//     tr.appendChild(td)
+//   }
+
+
+//   function addRow(tbl, val_1, val_2, val_3) {
+//     var tr = document.createElement('tr');
+
+//     addCell(tr, val_1);
+//     addCell(tr, val_2);
+//     addCell(tr, val_3);
+
+//     tbl.appendChild(tr)
+//   }
+
+//   function main() {
+//     tbl = document.getElementById('tbl');
+
+//     addRow(tbl, 'foo', 'bar', 'baz');
+//     addRow(tbl, 'one', 'two', 'three');
+//   }
+
+
+// var tr, td, i, oneRecord;
+//     var myDiv = document.createElement('DIV');
+//     myTable = document.createElement('TABLE');
+//     myTable.setAttribute("border", 1);
+//     // node tree
+//     var data = node.getElementsByTagName("NewDataSet")[0];
+//     for (i = 0; i < data.childNodes.length; i++) {
+        // use only 1st level element nodes to skip 1st level text nodes in NN
+//         if (data.childNodes[i].nodeType == 1) {
+//             // one final match record
+//              öneRecord = data.childNodes[i];
+//             tr = myTable.insertRow(myTable.rows.length);
+//             td = tr.insertCell(tr.cells.length);
+//             td.innerHTML = oneRecord.getElementsByTagName("Name")[0].firstChild.nodeValue;
+//             td = tr.insertCell(tr.cells.length);
+//             td.innerHTML = oneRecord.getElementsByTagName("Age")[0].firstChild.nodeValue;
+//             td = tr.insertCell(tr.cells.length);
+//             td.innerHTML = oneRecord.getElementsByTagName("Org")[0].firstChild.nodeValue;
+//         }
+//     }
+//     myDiv.appendChild(myTable);
+//     return myDiv.innerHTML;
+// }
